0932267d5519ba20290cf28501d6b167abe8e385
wrp1002 <wes.hamster@gmail.com>
2020-07-25 22:30:51 -0400

    cool

--- .theos/obj/debug/arm64/Tweak.x.m
+++ .theos/obj/debug/arm64/Tweak.x.m
@@ -45,150 +45,144 @@ void LogException(NSException *e) {
 @end
 
 
-
 @interface TVLock:NSObject {
 	UIWindow* springboardWindow;
 	UIView *mainView;
 	UIImageView *imageView;
 	UIView *whiteOverlay;
 }
-
-
-
-+ (id)sharedInstance;
--(id)init;
--(void)showLockAnimation:(float)arg1;
--(void)reset;
+	+ (id)sharedInstance;
+	-(id)init;
+	-(void)showLockAnimation:(float)arg1;
+	-(void)reset;
 @end
 
 
 @implementation TVLock
+	static id __strong _sharedObject;
 
-static id __strong _sharedObject;
+	+(id)sharedInstance {
+		Log(@"sharedInstance()");
 
-
-+(id)sharedInstance {
-	Log(@"sharedInstance()");
-
-	if (!_sharedObject) {
-		Log(@"sharedInstance() Creating new!");
-		_sharedObject = [[self alloc] init];
+		if (!_sharedObject) {
+			Log(@"sharedInstance() Creating new!");
+			_sharedObject = [[self alloc] init];
+		}
+		return _sharedObject;
 	}
-	return _sharedObject;
-}
 
--(id)init {
-	Log(@"init()");
-	self = [super init];
+	-(id)init {
+		Log(@"init()");
+		self = [super init];
 
-	if(self != nil) {
+		if(self != nil) {
+			@try {
+				springboardWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+				springboardWindow.windowLevel = UIWindowLevelStatusBar + 10000;
+				[springboardWindow setHidden:YES];
+				[springboardWindow _setSecure:YES];
+				springboardWindow.alpha = 1;
+				[springboardWindow setUserInteractionEnabled:NO];
+				
+				
+				
+				springboardWindow.backgroundColor = [UIColor blackColor];
+				
+				mainView = [[UIView alloc] initWithFrame:springboardWindow.bounds];
+				mainView.backgroundColor = [UIColor blackColor];
+				mainView.alpha = 1.0f;
+				mainView.layer.masksToBounds = YES;
+				[springboardWindow addSubview:mainView];
+
+				imageView = [[UIImageView alloc] initWithFrame:springboardWindow.bounds];
+				imageView.frame = springboardWindow.bounds;
+				imageView.contentMode = UIViewContentModeScaleAspectFill;
+				[mainView addSubview:imageView];
+
+				whiteOverlay = [[UIView alloc] initWithFrame:springboardWindow.bounds];
+				whiteOverlay.frame = springboardWindow.bounds;
+				whiteOverlay.backgroundColor = [UIColor whiteColor];
+				whiteOverlay.alpha = 0.0f;
+				whiteOverlay.layer.masksToBounds = YES;
+				[imageView addSubview:whiteOverlay];
+				
+			} @catch (NSException *e) {
+				LogException(e);
+			}
+		}
+		return self;
+	}
+
+	-(void)showLockAnimation:(float)speed {
+		Log(@"showLockAnimation()");
+		[self reset];
+		
 		@try {
-			springboardWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-			springboardWindow.windowLevel = UIWindowLevelStatusBar + 10000;
-			[springboardWindow setHidden:YES];
-			[springboardWindow _setSecure:YES];
-			springboardWindow.alpha = 1;
-			[springboardWindow setUserInteractionEnabled:NO];
 			
-			
-			
-			springboardWindow.backgroundColor = [UIColor blackColor];
-			
-			mainView = [[UIView alloc] initWithFrame:springboardWindow.bounds];
-			mainView.backgroundColor = [UIColor blackColor];
-			mainView.alpha = 1.0f;
-			mainView.layer.masksToBounds = YES;
-			[springboardWindow addSubview:mainView];
+			CGRect newFrameImage = CGRectMake(imageView.frame.origin.x,-imageView.center.y,imageView.frame.size.width,imageView.frame.size.height);
+			CGRect newFrameBack = CGRectMake(0,imageView.center.y,imageView.frame.size.width,5);
+			CGAffineTransform newTransformImage = CGAffineTransformScale(imageView.transform, 1, 0.5);
 
-			imageView = [[UIImageView alloc] initWithFrame:springboardWindow.bounds];
-			imageView.frame = springboardWindow.bounds;
-			imageView.contentMode = UIViewContentModeScaleAspectFill;
-			[mainView addSubview:imageView];
+			
+			extern UIImage *_UICreateScreenUIImage();
+			imageView.image = _UICreateScreenUIImage();
 
-			whiteOverlay = [[UIView alloc] initWithFrame:springboardWindow.bounds];
-			whiteOverlay.frame = springboardWindow.bounds;
-			whiteOverlay.backgroundColor = [UIColor whiteColor];
-			whiteOverlay.alpha = 0.0f;
 			
-			[imageView addSubview:whiteOverlay];
+			springboardWindow.hidden = NO;
 			
-		} @catch (NSException *e) {
+			[UIView animateWithDuration:speed*0.3 animations:^{
+
+				
+				imageView.frame = newFrameImage;
+				imageView.transform = newTransformImage;
+
+				mainView.frame = newFrameBack;
+
+				whiteOverlay.alpha = 1.0f;
+				
+
+			} completion:^(BOOL finished) {
+				dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+					[UIView animateWithDuration:speed*0.3 animations:^{
+
+						
+						mainView.frame = CGRectMake(imageView.bounds.size.width / 2, imageView.bounds.size.height / 2, 1, 1);
+
+					} completion:^(BOOL finished) {
+						dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+
+							[self reset];
+							
+						});
+					}];
+
+				});
+			}];
+		}
+		@catch (NSException *e) {
 			LogException(e);
 		}
+		
 	}
-	return self;
-}
 
--(void)showLockAnimation:(float)speed {
-	Log(@"showLockAnimation()");
-	
-	@try {
-		
-		CGRect newFrameImage = CGRectMake(imageView.frame.origin.x,-imageView.center.y,imageView.frame.size.width,imageView.frame.size.height);
-		CGRect newFrameBack = CGRectMake(0,imageView.center.y,imageView.frame.size.width,5);
-		CGAffineTransform newTransformImage = CGAffineTransformScale(imageView.transform, 1, 0.5);
+	-(void)reset {
+		Log(@"reset()");
 
-		
-		extern UIImage *_UICreateScreenUIImage();
-		imageView.image = _UICreateScreenUIImage();
+		mainView.alpha = 1.0f;
+		mainView.frame = springboardWindow.bounds;
+		mainView.transform = CGAffineTransformIdentity;
 
-		
-		springboardWindow.hidden = NO;
-		
-		[UIView animateWithDuration:speed*0.3 animations:^{
+		imageView.alpha = 1.0f;
+		imageView.frame = springboardWindow.bounds;
+		imageView.transform = CGAffineTransformIdentity;
+		imageView.image = nil;
 
-			
-			imageView.frame = newFrameImage;
-			imageView.transform = newTransformImage;
+		whiteOverlay.alpha = 0.0f;
+		whiteOverlay.frame = springboardWindow.bounds;
+		mainView.transform = CGAffineTransformIdentity;
 
-			mainView.frame = newFrameBack;
-
-			whiteOverlay.alpha = 1.0f;
-			
-
-		} completion:^(BOOL finished) {
-			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-				[UIView animateWithDuration:speed*0.3 animations:^{
-
-					
-					mainView.frame = CGRectMake(imageView.bounds.size.width / 2, imageView.bounds.size.height / 2, 1, 1);
-
-				} completion:^(BOOL finished) {
-					dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-
-						[self reset];
-						
-					});
-				}];
-
-			});
-		}];
+		springboardWindow.hidden = YES;
 	}
-	@catch (NSException *e) {
-		LogException(e);
-	}
-	
-}
-
--(void)reset {
-	Log(@"reset()");
-
-	mainView.alpha = 1.0f;
-	mainView.frame = springboardWindow.bounds;
-	mainView.transform = CGAffineTransformIdentity;
-
-	imageView.alpha = 1.0f;
-	imageView.frame = mainView.bounds;
-	imageView.transform = CGAffineTransformIdentity;
-	imageView.image = nil;
-
-	whiteOverlay.alpha = 0.0f;
-	whiteOverlay.frame = whiteOverlay.bounds;
-	mainView.transform = CGAffineTransformIdentity;
-
-	springboardWindow.hidden = YES;
-}
-
 
 @end
 
@@ -216,7 +210,7 @@ static id __strong _sharedObject;
 @class SpringBoard; @class SBBacklightController; 
 static void (*_logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$)(_LOGOS_SELF_TYPE_NORMAL SpringBoard* _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$(_LOGOS_SELF_TYPE_NORMAL SpringBoard* _LOGOS_SELF_CONST, SEL, id); static void (*_logos_orig$_ungrouped$SBBacklightController$_animateBacklightToFactor$duration$source$silently$completion$)(_LOGOS_SELF_TYPE_NORMAL SBBacklightController* _LOGOS_SELF_CONST, SEL, float, double, long long, BOOL, id); static void _logos_method$_ungrouped$SBBacklightController$_animateBacklightToFactor$duration$source$silently$completion$(_LOGOS_SELF_TYPE_NORMAL SBBacklightController* _LOGOS_SELF_CONST, SEL, float, double, long long, BOOL, id); 
 
-#line 194 "Tweak.x"
+#line 188 "Tweak.x"
 
 
 	
@@ -253,10 +247,10 @@ static void (*_logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$)
 
 
 
-static __attribute__((constructor)) void _logosLocalCtor_912512e6(int __unused argc, char __unused **argv, char __unused **envp) {
+static __attribute__((constructor)) void _logosLocalCtor_61f2793c(int __unused argc, char __unused **argv, char __unused **envp) {
 	
 	
 }
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$SpringBoard = objc_getClass("SpringBoard"); { MSHookMessageEx(_logos_class$_ungrouped$SpringBoard, @selector(applicationDidFinishLaunching:), (IMP)&_logos_method$_ungrouped$SpringBoard$applicationDidFinishLaunching$, (IMP*)&_logos_orig$_ungrouped$SpringBoard$applicationDidFinishLaunching$);}Class _logos_class$_ungrouped$SBBacklightController = objc_getClass("SBBacklightController"); { MSHookMessageEx(_logos_class$_ungrouped$SBBacklightController, @selector(_animateBacklightToFactor:duration:source:silently:completion:), (IMP)&_logos_method$_ungrouped$SBBacklightController$_animateBacklightToFactor$duration$source$silently$completion$, (IMP*)&_logos_orig$_ungrouped$SBBacklightController$_animateBacklightToFactor$duration$source$silently$completion$);}} }
-#line 234 "Tweak.x"
+#line 228 "Tweak.x"
--- Tweak.x
+++ Tweak.x
@@ -44,150 +44,144 @@ void LogException(NSException *e) {
 @end
 
 
-
 @interface TVLock:NSObject {
 	UIWindow* springboardWindow;
 	UIView *mainView;
 	UIImageView *imageView;
 	UIView *whiteOverlay;
 }
-/*@property (nonatomic, strong) UIWindow* springboardWindow;
-@property (nonatomic, strong) UIView *mainView;
-@property (nonatomic, strong) UIImageView *imageView;*/
-+ (id)sharedInstance;
--(id)init;
--(void)showLockAnimation:(float)arg1;
--(void)reset;
+	+ (id)sharedInstance;
+	-(id)init;
+	-(void)showLockAnimation:(float)arg1;
+	-(void)reset;
 @end
 
 
 @implementation TVLock
-//@synthesize springboardWindow, mainView, imageView;
-static id __strong _sharedObject;
+	static id __strong _sharedObject;
 
+	+(id)sharedInstance {
+		Log(@"sharedInstance()");
 
-+(id)sharedInstance {
-	Log(@"sharedInstance()");
-
-	if (!_sharedObject) {
-		Log(@"sharedInstance() Creating new!");
-		_sharedObject = [[self alloc] init];
+		if (!_sharedObject) {
+			Log(@"sharedInstance() Creating new!");
+			_sharedObject = [[self alloc] init];
+		}
+		return _sharedObject;
 	}
-	return _sharedObject;
-}
 
--(id)init {
-	Log(@"init()");
-	self = [super init];
+	-(id)init {
+		Log(@"init()");
+		self = [super init];
 
-	if(self != nil) {
+		if(self != nil) {
+			@try {
+				springboardWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+				springboardWindow.windowLevel = UIWindowLevelStatusBar + 10000;
+				[springboardWindow setHidden:YES];
+				[springboardWindow _setSecure:YES];
+				springboardWindow.alpha = 1;
+				[springboardWindow setUserInteractionEnabled:NO];
+				//springboardWindow.layer.cornerRadius = 1.0f;
+				//springboardWindow.layer.masksToBounds = YES;
+				//springboardWindow.layer.shouldRasterize  = NO;
+				springboardWindow.backgroundColor = [UIColor blackColor];
+				
+				mainView = [[UIView alloc] initWithFrame:springboardWindow.bounds];
+				mainView.backgroundColor = [UIColor blackColor];
+				mainView.alpha = 1.0f;
+				mainView.layer.masksToBounds = YES;
+				[springboardWindow addSubview:mainView];
+
+				imageView = [[UIImageView alloc] initWithFrame:springboardWindow.bounds];
+				imageView.frame = springboardWindow.bounds;
+				imageView.contentMode = UIViewContentModeScaleAspectFill;
+				[mainView addSubview:imageView];
+
+				whiteOverlay = [[UIView alloc] initWithFrame:springboardWindow.bounds];
+				whiteOverlay.frame = springboardWindow.bounds;
+				whiteOverlay.backgroundColor = [UIColor whiteColor];
+				whiteOverlay.alpha = 0.0f;
+				whiteOverlay.layer.masksToBounds = YES;
+				[imageView addSubview:whiteOverlay];
+				
+			} @catch (NSException *e) {
+				LogException(e);
+			}
+		}
+		return self;
+	}
+
+	-(void)showLockAnimation:(float)speed {
+		Log(@"showLockAnimation()");
+		[self reset];
+		
 		@try {
-			springboardWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-			springboardWindow.windowLevel = UIWindowLevelStatusBar + 10000;
-			[springboardWindow setHidden:YES];
-			[springboardWindow _setSecure:YES];
-			springboardWindow.alpha = 1;
-			[springboardWindow setUserInteractionEnabled:NO];
-			//springboardWindow.layer.cornerRadius = 1.0f;
-			//springboardWindow.layer.masksToBounds = YES;
-			//springboardWindow.layer.shouldRasterize  = NO;
-			springboardWindow.backgroundColor = [UIColor blackColor];
-			
-			mainView = [[UIView alloc] initWithFrame:springboardWindow.bounds];
-			mainView.backgroundColor = [UIColor blackColor];
-			mainView.alpha = 1.0f;
-			mainView.layer.masksToBounds = YES;
-			[springboardWindow addSubview:mainView];
+			//	Prepare for first animation
+			CGRect newFrameImage = CGRectMake(imageView.frame.origin.x,-imageView.center.y,imageView.frame.size.width,imageView.frame.size.height);
+			CGRect newFrameBack = CGRectMake(0,imageView.center.y,imageView.frame.size.width,5);
+			CGAffineTransform newTransformImage = CGAffineTransformScale(imageView.transform, 1, 0.5);
 
-			imageView = [[UIImageView alloc] initWithFrame:springboardWindow.bounds];
-			imageView.frame = springboardWindow.bounds;
-			imageView.contentMode = UIViewContentModeScaleAspectFill;
-			[mainView addSubview:imageView];
+			// Get screenshot
+			extern UIImage *_UICreateScreenUIImage();
+			imageView.image = _UICreateScreenUIImage();
 
-			whiteOverlay = [[UIView alloc] initWithFrame:springboardWindow.bounds];
-			whiteOverlay.frame = springboardWindow.bounds;
-			whiteOverlay.backgroundColor = [UIColor whiteColor];
-			whiteOverlay.alpha = 0.0f;
-			//whiteOverlay.layer.masksToBounds = YES;
-			[imageView addSubview:whiteOverlay];
+			//	Show animation window
+			springboardWindow.hidden = NO;
 			
-		} @catch (NSException *e) {
+			[UIView animateWithDuration:speed*0.3 animations:^{
+
+				//	First part of animation
+				imageView.frame = newFrameImage;
+				imageView.transform = newTransformImage;
+
+				mainView.frame = newFrameBack;
+
+				whiteOverlay.alpha = 1.0f;
+				
+
+			} completion:^(BOOL finished) {
+				dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+					[UIView animateWithDuration:speed*0.3 animations:^{
+
+						//	Second part of animation
+						mainView.frame = CGRectMake(imageView.bounds.size.width / 2, imageView.bounds.size.height / 2, 1, 1);
+
+					} completion:^(BOOL finished) {
+						dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+
+							[self reset];
+							
+						});
+					}];
+
+				});
+			}];
+		}
+		@catch (NSException *e) {
 			LogException(e);
 		}
-	}
-	return self;
-}
-
--(void)showLockAnimation:(float)speed {
-	Log(@"showLockAnimation()");
-	
-	@try {
-		//	Prepare for first animation
-		CGRect newFrameImage = CGRectMake(imageView.frame.origin.x,-imageView.center.y,imageView.frame.size.width,imageView.frame.size.height);
-		CGRect newFrameBack = CGRectMake(0,imageView.center.y,imageView.frame.size.width,5);
-		CGAffineTransform newTransformImage = CGAffineTransformScale(imageView.transform, 1, 0.5);
-
-		// Get screenshot
-		extern UIImage *_UICreateScreenUIImage();
-		imageView.image = _UICreateScreenUIImage();
-
-		//	Show animation window
-		springboardWindow.hidden = NO;
 		
-		[UIView animateWithDuration:speed*0.3 animations:^{
-
-			//	First part of animation
-			imageView.frame = newFrameImage;
-			imageView.transform = newTransformImage;
-
-			mainView.frame = newFrameBack;
-
-			whiteOverlay.alpha = 1.0f;
-			
-
-		} completion:^(BOOL finished) {
-			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-				[UIView animateWithDuration:speed*0.3 animations:^{
-
-					//	Second part of animation
-					mainView.frame = CGRectMake(imageView.bounds.size.width / 2, imageView.bounds.size.height / 2, 1, 1);
-
-				} completion:^(BOOL finished) {
-					dispatch_after(dispatch_time(DISPATCH_TIME_NOW, speed*0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-
-						[self reset];
-						
-					});
-				}];
-
-			});
-		}];
 	}
-	@catch (NSException *e) {
-		LogException(e);
+
+	-(void)reset {
+		Log(@"reset()");
+
+		mainView.alpha = 1.0f;
+		mainView.frame = springboardWindow.bounds;
+		mainView.transform = CGAffineTransformIdentity;
+
+		imageView.alpha = 1.0f;
+		imageView.frame = springboardWindow.bounds;
+		imageView.transform = CGAffineTransformIdentity;
+		imageView.image = nil;
+
+		whiteOverlay.alpha = 0.0f;
+		whiteOverlay.frame = springboardWindow.bounds;
+		mainView.transform = CGAffineTransformIdentity;
+
+		springboardWindow.hidden = YES;
 	}
-	
-}
-
--(void)reset {
-	Log(@"reset()");
-
-	mainView.alpha = 1.0f;
-	mainView.frame = springboardWindow.bounds;
-	mainView.transform = CGAffineTransformIdentity;
-
-	imageView.alpha = 1.0f;
-	imageView.frame = mainView.bounds;
-	imageView.transform = CGAffineTransformIdentity;
-	imageView.image = nil;
-
-	whiteOverlay.alpha = 0.0f;
-	whiteOverlay.frame = whiteOverlay.bounds;
-	mainView.transform = CGAffineTransformIdentity;
-
-	springboardWindow.hidden = YES;
-}
-
 
 @end
 
